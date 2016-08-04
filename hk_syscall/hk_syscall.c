1de2f92663975d03fff5c2b3bed77c02a43fabf5
dawter <dawter@qq.com>
2016-08-04 23:29:45 +0800

    finish x86_64 hook

--- hk_syscall/hk_syscall.c
+++ hk_syscall/hk_syscall.c
@@ -1,10 +1,13 @@
 /*
- * hk_syscall: hook syscall
+ * hk_syscall: hook syscall on x86_64 linux
+ * test and build system centos-6.9 2.6.32
  * Wed Jul 20 12:09:49 CST 2016
  */
 #include <linux/module.h> /* needed by all modules */
 #include <linux/kernel.h> /* needed for KERN_NFO */
 #include <linux/limits.h> /* needed for macro PATH_MAX */
+#include <linux/uaccess.h>   /* access_ok() */
+#include <asm/unistd.h>   /* micro  __NR_chmod  */
 #include <linux/moduleparam.h> /* needed for module_param */
 
 static char hk_path[PATH_MAX];
@@ -12,8 +15,44 @@ static char *path = "/tmp/hk_dir/";
 module_param(path, charp, 000);
 MODULE_PARM_DESC(path, "A directory to test hook");
 
-extern void *syscall_table[];
+void **syscall_table;
+/* refer to linux/syscalls.h */
 asmlinkage long   (*orig_chmod)(const char __user *, mode_t);
+
+asmlinkage long hk_chmod(const char __user *filename, mode_t mode)
+{
+	long len;
+	long retval = 0;
+	char *buffer;
+
+	retval = access_ok(VERIFY_READ, filename, 3);
+	if(0 == retval) {
+		printk(KERN_NOTICE "chmod: access_ok(filename) failed\n");
+		return orig_chmod(filename, mode);
+	}
+	len = strlen_user(filename);
+	if(0 == len) {
+		printk(KERN_NOTICE "chmod: strlen_user(filename) failed\n");
+		return orig_chmod(filename, mode);
+	}
+	buffer = kmalloc(PATH_MAX, GFP_KERNEL);
+	if(NULL == buffer) {
+		printk(KERN_ERR "chmod kmalloc failed");
+		return orig_chmod(filename, mode);
+	}
+
+	retval = strncpy_from_user(buffer, filename, len);
+
+	if(strncmp(hk_path, buffer, strlen(hk_path)) == 0) {
+		printk(KERN_NOTICE "chmod: forbidden\n");
+		retval = -1;
+	} else {
+		retval = orig_chmod(filename, mode);
+	}
+
+	kfree(buffer);
+	return retval;
+}
 
 static void *memmem(const void *haystack, size_t haystack_len, const void *needle, size_t needle_len) 
 {
@@ -55,21 +94,68 @@ void* get_syscall_addr(void)
 		printk("long mode: memmem found nothing, return NULL\n");
 	}
 
-
 	return (void*)retval;
 }
+
+unsigned long cr0_cnt(void)
+{
+	unsigned long ret;
+
+	asm volatile ( "movq %%cr0, %0"
+			:"=r"(ret)
+			:
+		     );
+
+	asm volatile ( "movq %0, %%cr0"
+			:
+			:"r"(ret&0xfffffffffffeffff)
+		     );
+	return ret;
+}
+
+void cr0_restore(unsigned long val)
+{
+	asm volatile ( "movq %0, %%cr0"
+			:
+			:"r"(val)
+		     );
+}
+
 
 int hk_syscall(void)
 {
 	int retval = 0;
+
+	unsigned long orig_cr0;
 	syscall_table = get_syscall_addr();
 	if(NULL == syscall_table) {
 		printk("can not get syscall addr\n");
+		retval = 1;
 	}
 	printk("syscall addr: %p\n", syscall_table);
-	
+
+	orig_chmod = syscall_table[__NR_chmod];
+	printk("orig_chmod: %p\n", orig_chmod);
+
+	orig_cr0 = cr0_cnt();
+	syscall_table[__NR_chmod] = hk_chmod;
+	cr0_restore(orig_cr0);
+
 	return retval;
 }
+
+int restore_syscall(void)
+{
+	int retval = 0;
+	unsigned long orig_cr0;
+
+	orig_cr0 = cr0_cnt();
+	syscall_table[__NR_chmod] = orig_chmod;
+	cr0_restore(orig_cr0);
+
+	return retval;
+}
+
 static int __init hk_syscall_init(void)
 {
 	int ret = 0;
@@ -79,15 +165,14 @@ static int __init hk_syscall_init(void)
 	}
 	strcpy(hk_path, path);
 	printk(KERN_INFO "hook path: %s\n", hk_path);
-	printk(KERN_INFO "syscall_table: %p\n", syscall_table);
-
-	hk_syscall();
+	ret = hk_syscall();
 	return ret;
 }
 
 static void __exit hk_syscall_exit(void)
 {
 	printk(KERN_INFO "exit module hk_syscall\n");
+	restore_syscall();
 	return;
 }
 
